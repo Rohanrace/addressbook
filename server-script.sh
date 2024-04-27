d5aa73cdfdf2004bd363f2410267deffca214fb8
Rohanrace <160409965+Rohanrace@users.noreply.github.com>
2024-04-27 05:33:52 +0530

    Update server-script.sh

--- server-script.sh
+++ server-script.sh
@@ -12,7 +12,7 @@ then
    cd /home/ec2-user/addressbook
    git pull origin april-eks
 else
-   git clone https://github.com/rohanrace/addressbook.git
+   git clone https://github.com/Rohanrace/addressbook.git
    cd addressbook
    git checkout april-eks
 fi
